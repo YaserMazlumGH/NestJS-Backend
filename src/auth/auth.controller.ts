67fdef90aad80f4909dfd88504e33453bc987bfe
Manu <yasermazlum@gmail.com>
2022-08-30 22:01:33 +0300

    last

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { AuthDto } from './dto';
 import { AuthService } from './auth.service';
-import { Body, Controller, Post, Req } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Get, Post, Req } from "@nestjs/common";
 import { Request } from 'express';
 
 @Controller('auth')
@@ -8,13 +8,18 @@ export class AuthController{
     constructor(private readonly authService:AuthService){}
     
     @Post('signin')
-    login(){
-        return {msg:'signin calıstı'}
+    login(@Body() dto:AuthDto){
+        this.authService.login(dto);
     }
 
     @Post('signup')
     signup(@Body() dto:AuthDto){
-        this.authService.login(dto);
+        this.authService.signUp(dto);
+    }
+
+    @Get('getlist')
+    getlist(){
+        return 'liste getirildi'
     }
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,29 @@
 import { AuthDto } from './dto/auth.dto';
 import { PrismaService } from './../prisma/prisma.service';
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import * as argon from 'argon2'
 
 @Injectable({})
-export class AuthService{
+export class AuthService {
 
-    constructor(private readonly prisma:PrismaService){}
+    constructor(private readonly prisma: PrismaService) { }
 
-    login(dto:AuthDto){
-        console.log({dto})
-        return {mesaj:"Giriş yapıldı."}
+    async login(dto: AuthDto) {
+
+        const user = await this.prisma.user.findFirst();
+        
+        const decodePassword = await argon.verify(user.passwordHash, dto.password)
+        console.log(decodePassword)
+
+
+        if(!decodePassword) throw new ForbiddenException("Böyle bir kullanıcı yok")
+
+        delete user.passwordHash;
+
+        return user;
     }
 
-    async signUp(dto:AuthDto){
+    async signUp(dto: AuthDto) {
         const passwordHash = await argon.hash(dto.password)
         const user = await this.prisma.user.create({
             data: {
